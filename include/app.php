4a5b71b0d55103a8709288a15228dc55b7ca9169
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2021-03-12 01:02:51 +0100

    agi1: Refactor the app
    This was too ugly and I'm kinda bored at 1am so yeah. Anyway:
    
    -Move everything to a separate file
        *Make everything part of App class
    -Divide all functionality into few smaller functions
    -Remove redundant code and add neccessary comments
    
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

---
+++ include/app.php
@@ -0,0 +1,100 @@
+<?php
+
+class App
+{
+    function getCallerID() {
+        global $agi;
+
+        return $agi->parse_callerid()['username'];
+    }
+
+    function connectDB($host, $user, $pw, $db) {
+        $conn = new mysqli($host, $user, $pw, $db);
+        if (mysqli_connect_error())
+            die("Couldn't connect to database");
+        return $conn;
+    }
+
+    function currentUser() {
+        global $callerID, $db;
+
+        $query = 'SELECT password FROM users WHERE extension = "' . $callerID . '"';
+        $existingUser = $db->query($query);
+        if (!$existingUser)
+            die('Query failed');
+
+        if ($existingUser->num_rows > 0)
+            return $existingUser->fetch_assoc()['password'];
+
+        return false;
+    }
+
+    function authenticate($matchedPw) {
+        //In case currentUser function returned false aka this user is logging in for the first time
+        if (!$matchedPw)
+            return;
+
+        global $agi;
+
+        $pw = $agi->get_data('agent-pass', -1)['result'];
+        //Just leave if login failed, alternatively loop until we hit the pw
+        if ($pw !== $matchedPw) {
+            $agi->stream_file('vm-incorrect');
+            die();
+        }
+    }
+
+    function getChoice() {
+        global $agi;
+
+        return $agi->get_data('conf-getchannel', -1, 1)['result'];
+    }
+
+    function requestCall() {
+        global $agi, $callerID;
+
+        $ext = $agi->get_data('vm-enter-num-to-call', -1)['result'];
+
+        $agi->stream_file($ext === $callerID ? 'pbx-invalid' : 'followme/pls-hold-while-try');
+        if ($ext !== $callerID) {
+            // $status = $agi->exec_dial('PJSIP', $ext)['result'];
+            $status = $agi->exec_dial('PJSIP', 'SOFTPHONE_B')['result']; //Call SOFTPHONE_B set up on twinkle, for testing
+            if ($status !== 200)
+                return -1;
+        }
+        return 0;
+    }
+
+    function changePassword() {
+        global $agi, $db, $callerID, $userPw;
+
+        $pw = $agi->get_data('agent-pass', -1)['result'];
+        if ($userPw) {
+            //This user already has an account, go on
+            $query = 'SELECT 1 FROM users WHERE extension = "' . $callerID . '" AND password = "' . $pw . '"';
+            $match = $db->query($query);
+            if (!$match->num_rows) {
+                $agi->stream_file('vm-invalidpassword');
+                return -1;
+            }
+
+            //Double confirm new password before updating db
+            $newPw = $agi->get_data('vm-newpassword', -1)['result'];
+            $newPwConfirm = $agi->get_data('vm-reenterpassword', -1)['result'];
+            if ($newPw !== $newPwConfirm) {
+                $agi->stream_file('vm-mismatch');
+                return -1;
+            }
+        }
+        //If first time around, create entry in db, else update existing row in table
+        $query = $userPw ? 'UPDATE users SET password = "' . $newPw . '" WHERE extension = "' . $callerID . '"'
+            : 'INSERT INTO users values ("' . $callerID . '", "' . $pw . '")';
+
+        if (!$db->query($query))
+            die('Query failed');
+
+        $agi->stream_file('vm-passchanged');
+
+        return 0;
+    }
+}
--- src/agi1.php
+++ src/agi1.php
@@ -1,105 +1,42 @@
 #!/usr/bin/php -q
 <?php
-include('phpagi.php');
+include('/home/zlatan/projects/AGI/include/phpagi.php');
+include('/home/zlatan/projects/AGI/include/app.php');
 $agi = new AGI();
+$app = new App();
+
+//Connect to db
+$db = $app->connectDB("localhost", "asterisk", "test1234", "demo1");
 
 //Fetch extension, needed later
-$caller_id = $agi->parse_callerid()['username'];
+$callerID = $app->getCallerID();
 
 //Pick up the call
 $agi->answer();
-
-//Connect to db
-$db = new mysqli('localhost', 'asterisk', 'test123', 'demo1');
-if (mysqli_connect_error()) {
-    $agi->verbose("Couldn't connect to database");
-    exit();
-}
-
-//Check if there is entry for this user in db
-$query = 'SELECT password FROM users WHERE extension = "' . $caller_id . '"';
-$existing_user = $db->query($query);
-if (!$existing_user) {
-    $agi->verbose('Query failed');
-    goto end;
-}
-if ($existing_user->num_rows > 0) {
-    $pw = $agi->get_data('agent-pass', -1)['result'];
-    $user_pw = $existing_user->fetch_assoc()['password'];
-
-    //Just leave if login failed, alternatively loop until we hit the pw
-    if ($pw !== $user_pw) {
-        $agi->stream_file('vm-incorrect');
-        exit();
-    }
-}
-
 $agi->stream_file('hello-world');
+
+$userPw = $app->currentUser();
+$app->authenticate($userPw);
 
 for (;;) {
     $agi->stream_file('dir-welcome');
-    $choice = $agi->get_data('conf-getchannel', -1, 1)['result'];
+    $choice = $app->getChoice();
 
-    switch ($choice) {
-        case 1:
-            $ext = $agi->get_data('vm-enter-num-to-call', -1)['result'];
-            if ($ext === $caller_id) {
-                //Not allowed to call myself, just go on
-                $agi->stream_file('pbx-invalid');
-            } else {
-                $agi->stream_file('followme/pls-hold-while-try');
-                // $status = $agi->exec_dial('PJSIP', $ext)['result']; //What if ext doesn't exist?
-                $status = $agi->exec_dial('PJSIP', 'SOFTPHONE_B')['result']; //Call twinkle, test
-                if ($status !== 200) {
-                    $agi->verbose('Call failed!');
-                    goto end;
-                }
-            }
-            break;
-        case 2:
-            $pw = $agi->get_data('agent-pass', -1)['result'];
-            if ($user_pw) {
-                //This user already has an account, go on
-                $query = 'SELECT 1 FROM users WHERE extension = "' . $caller_id . '" AND password = "' . $pw . '"';
-                $match = $db->query($query);
-                if ($match->num_rows === 0) {
-                    $agi->stream_file('vm-invalidpassword');
-                    continue;
-                }
-
-                //Double confirm new password before updating db
-                $newPw = $agi->get_data('vm-newpassword', -1)['result'];
-                $pw_confirm = $agi->get_data('vm-reenterpassword', -1)['result'];
-                if ($newPw !== $pw_confirm) {
-                    $agi->stream_file('vm-mismatch');
-                    continue;
-                }
-            }
-            //If first time around, create entry in db
-            $query = $user_pw ? 'UPDATE users SET password = "' . $newPw . '" WHERE extension = "' . $caller_id . '"'
-                : 'INSERT INTO users values ("' . $caller_id . '", "' . $pw . '")';
-            $ret = $db->query($query);
-            if (!$ret) {
-                $agi->verbose('Query failed');
-                goto end;
-            }
-
-            $agi->stream_file('vm-passchanged');
-            break;
-        case 3:
-            //Repeat
-            continue;
-        case 9:
-            //End
-            goto end;
-        default:
-            //Bad choice
-            $agi->stream_file('conf-errormenu');
-            break;
-    }
+    if ($choice == 1) {
+        if ($app->requestCall())
+            die('Call failed');
+    } else if ($choice == 2) {
+        $app->changePassword();
+    } else if ($choice == 3)
+        //Repeat
+        continue;
+    else if ($choice == 9)
+        //End
+        break;
+    else
+        $agi->stream_file('conf-errormenu');
 }
 
-end:
 $agi->stream_file('demo-thanks');
 $agi->hangup();
 ?>
